54a09f11889de592db46a24dcb95c09d21bdc222
khalil trabelsi <khaliltrabelsi2107@gmail.com>
2025-09-14 14:18:06 +0200

    add log when user access the app

--- src/routes/portfolio.py
+++ src/routes/portfolio.py
@@ -1,7 +1,7 @@
 import json
 
 import requests
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, current_app
 
 from src.extensions import db
 from src.models import Visitor
@@ -13,8 +13,11 @@ portfolio_bp = Blueprint('portfolio', __name__)
 def index():
     remote_addr = request.remote_addr
     visitor_infos = requests.get(f"https://ipapi.co/{remote_addr}/json/").json()
+    current_app.logger.info(f'New visitor -: {visitor_infos}')
+    print(f'New visitor -: {visitor_infos}')
 
-    visitor = Visitor(ip=remote_addr, user_agent=request.headers.get('User-Agent'), description=json.dumps(visitor_infos))
+    visitor = Visitor(ip=remote_addr, user_agent=request.headers.get('User-Agent'),
+                      description=json.dumps(visitor_infos))
 
     db.session.add(visitor)
     db.session.commit()
